127545937aacb9fa5c48a489729c3f10e39bec8f
LEE KUAN CHAN <kcli0124@gmail.com>
2026-01-07 10:01:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js - 修正版 (2026/01/07)
+// script.js - 前端專用版 (2026/01/07)
 
 const API_BASE_URL = "https://mariyah-unexplanatory-regan.ngrok-free.dev";
 
@@ -16,7 +16,7 @@ async function callAPI(endpoint, method = 'POST', body = null) {
   try {
     const headers = {
       'Content-Type': 'application/json',
-      'ngrok-skip-browser-warning': '69420'  // 跳過 ngrok 警告
+      'ngrok-skip-browser-warning': '69420'
     };
 
     const options = { method, headers };
